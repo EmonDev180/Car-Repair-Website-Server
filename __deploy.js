db4829e1ac1629fe1e3667ec32743ffae23c3c3e
Emon Dev180 <shuvroemon.cse@gmail.com>
2024-07-30 21:41:33 +0600

    first commit

---
+++ __deploy.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ *  * Server side deploy
+ * 1. vercel config
+ * 2. if you use cookies in cross site: use cors for your production url
+ * 3. environment variable
+ * 4.server :vercel --prod
+
+ */
--- index.js
+++ index.js
@@ -2,22 +2,29 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
-const cookieParse = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // midleWere 
 
 app.use(cors({
-  origin:['http://localhost:5173'],
+  origin:[
+       'http://localhost:5173',
+      //  'https://car-doctor-server-eta-plum.vercel.app',
+      // 'https://car-doctor-10f7f.web.app',
+      // 'https://car-doctor-10f7f.firebaseapp.com',
+   
+  ],
   credentials:true
 }));
 app.use(express.json());
+app.use(cookieParser())
 
-app.use(cookieParse())
+
 
 
 console.log(process.env.DB_USER);
@@ -36,32 +43,73 @@ const client = new MongoClient(uri, {
   }
 });
 
+//middleweres
+
+const logger = async(req,res,next) =>{
+
+  console.log('called',req.host,req.originalUrl)
+  next();
+
+}
+const varifyToken = (req,res,next) => {
+  const token = req?.cookies?.token;
+
+  console.log('token in the middeleWere',token);
+
+
+  if(!token) {
+    return res.status(401).send({message:'Unauthorized access'})
+  }
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=> {
+    if(err) {
+      return res.status(401).send({message:'Unauthrized access'})
+    }
+    req.user = decoded;
+  })
+
+  next()
+
+
+}
+
+
+
+
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
 
     const serviceCollection = client.db('carDoctor').collection('services')
     const bookingCollection = client.db('carDoctor').collection('booking')
+    const productCollection = client.db('carDoctor').collection('products')
 
     //Auth related api
 
-    app.post('/jwt',async(req,res) => {
+    app.post('/jwt',logger,async(req,res) => {
       const user = req.body;
 
       console.log(user)
-      
+
       const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
         expiresIn:'1h'
-      })
-
+      });
   
       res
       .cookie('token',token,{
         httpOnly:true,
-        secure: false
+        secure:false
 
       })
+
+
+
+
+
+  
       
       .send({success:true});
 
@@ -70,11 +118,20 @@ async function run() {
     })
 
 
+    // products related 
+
+    app.get('/products',async(req,res) => {
+
+      const result = await productCollection.find().toArray()
+
+      res.send(result);
+    })
+
 
 
     // services related 
 
-    app.get('/services', async (req, res) => {
+    app.get('/services',logger,   async (req, res) => {
 
       const cursor = serviceCollection.find();
 
@@ -107,23 +164,36 @@ async function run() {
     // booking 
 
     app.get('/bookings', async (req, res) => {
-      console.log(req.query.email);
+      
 
-      console.log('tttt token ',req.cookies.token)
+      // console.log(req.query.email)
+
+      // console.log('token ower info',req.user)
+      // if( req.user.email  !== req.query.email){
+      //   return res.status(403).send({message:'forbiden access'})
+      // }
+
+      
+      // let query = {};
+
+      // if (req.query?.email) {
+      //   query = { email: req.query.email }
+      // }
 
 
- 
+      // const cursor = bookingCollection.find(query);
 
-      let query = {};
+      // const result = await cursor.toArray();
 
-      if (req.query?.email) {
-        query = { email: req.query.email }
-      }
+      // res.send(result)
+
+      const email = req.query.email;
+
+      const query = {email: email}
 
 
-      const cursor = bookingCollection.find(query);
 
-      const result = await cursor.toArray();
+      const  result = await bookingCollection.find(query).toArray()
 
       res.send(result)
 
@@ -166,16 +236,6 @@ async function run() {
       res.send(result);
 
 
-
-
-
-
-
-
-
-
-
-
     })
 
 
@@ -202,11 +262,11 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    //await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
